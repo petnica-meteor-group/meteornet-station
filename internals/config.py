1a0eebe693c0abc59844bf4ce543684ec06063f7
schutzekatze <vladimir.nik94@gmail.com>
2019-12-13 23:41:21 -0800

    Bump version

--- internals/config.py
+++ internals/config.py
@@ -19,7 +19,7 @@ else:
 PROJECT_PATH = dirname(dirname(__file__))
 MAIN_FILENAME = 'start.py'
 
-VERSION = '1.0.2.3'
+VERSION = '1.0.3.0'
 
 STATION_INFO_FILENAME = 'station_info.cfg'
 STATION_INFO_FILEPATH = join(PROJECT_PATH, STATION_INFO_FILENAME)
